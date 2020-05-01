24d798bc31687dc058de783a245e49ba72730d9d
deeg05 <45331856+deeg05@users.noreply.github.com>
2020-05-01 19:17:52 +0300

    Create main.h

---
+++ src/main.h
@@ -0,0 +1,14 @@
+void ledBlink();
+
+void connectWifi();
+void handleRoot();
+
+void lightsOff();
+void lightsOn();
+
+int debouncePin(void);
+
+int lightbulb;
+
+int ButtonStateOld;
+int ButtonState;
